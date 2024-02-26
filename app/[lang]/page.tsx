f2a5c3aad81768faa13e61ce77ac1ccc70ffeb9f
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2024-02-26 05:57:07 -0330

    add carousel to the home page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,9 +1,9 @@
 import { Locale } from "@/i18n.config"
-import Sliders from "@/components/layout/sliders"
 import Footer from "@/components/footer"
 import { getDictionary } from "@/lib/locale"
 import NavBar from "@/components/layout/nav/navbar"
 import HomeAbout from "@/components/home/about-us"
+import CarouselWithContent from "@/components/layout/carousel"
 
 const Home = async({
   params: { lang }
@@ -12,11 +12,10 @@ const Home = async({
 }) => {
  
   const { navigation } = await getDictionary(lang);
-
   return (
     <div className="mt-0 w-full max-h-screen overflow-scroll">
       <NavBar lang={lang} navigation={navigation}/>
-      <Sliders />
+      <CarouselWithContent />
       <HomeAbout/>
       <Footer />
     </div>
