436be4eeb2970c5225b75a8073aac291635cf55d
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2024-03-01 23:55:30 +0330

    fix the dashboard for mobile size

---
+++ app/[lang]/dashboard/components/avatat.tsx
@@ -0,0 +1,12 @@
+import { Avatar } from "@material-tailwind/react"
+
+const UserAvatar = () => {
+  return (
+    <Avatar
+      src="https://docs.material-tailwind.com/img/face-2.jpg"
+      alt="avatar"
+    />
+  )
+}
+
+export default UserAvatar
--- app/[lang]/dashboard/page.tsx
+++ app/[lang]/dashboard/page.tsx
@@ -1,23 +1,27 @@
-"use client";
+"use client"
 
-import React from 'react'
-import { SidebarWithSearch } from './components/sidebar'
-import { NavbarWithSearch } from './components/navbar'
-import { Card } from '@material-tailwind/react'
+import React from "react"
+import Sidebar from "./components/sidebar"
+import DashboardNavbar from "./components/navbar"
+import { Card } from "@material-tailwind/react"
 
 const Page = () => {
-  return (
-    <section className="bg-light-800 min-h-screen">
-       <div className="flex sm:flex-row flex-col p-2">
-          <SidebarWithSearch/>
-          <div className="flex-1 w-full px-5">
-             <NavbarWithSearch/>
-             <Card className="mt-6 w-full rounded-md h-[40rem]">
-               page content
-             </Card>
-          </div>
-       </div>
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false)
+  const openDrawer = () => setIsDrawerOpen(true)
+  const closeDrawer = () => setIsDrawerOpen(false)
 
+  return (
+    <section className="min-h-screen bg-light-800">
+      <div className="flex flex-col p-2 sm:flex-row">
+        <Sidebar closeDrawer={closeDrawer} isDrawerOpen={isDrawerOpen} />
+        <div className="w-full flex-1 px-5">
+          <DashboardNavbar
+            isDrawerOpen={isDrawerOpen}
+            openDrawer={openDrawer}
+          />
+          <Card className="mt-6 h-[40rem] w-full rounded-md">page content</Card>
+        </div>
+      </div>
     </section>
   )
 }
